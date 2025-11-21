bdcab2868aefbddf4cd5a4245c08d291b39fa3a6
Christine Shim <christine.shim1123@gmail.com>
2025-11-20 21:46:06 -0500

    hi

--- src/application/blinky/blinky.c
+++ src/application/blinky/blinky.c
@@ -17,8 +17,10 @@ void blinky_task(void *pvParameters)
         // ------ onboarding task part 0.5 ------
         // toggle green led on/off each time this loop runs (which is connected to gpio PE10)
         HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_10);
+        HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_9);
+        HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_11);
         // delay for 500 ms
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(100));
         // --------------------------------------
 
         // ------ onboarding task part 1 ------
